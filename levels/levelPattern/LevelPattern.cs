2e5d9f86812ebbe41c3080c23d43c965150ac260
berón <bleink@outlook.es>
2023-01-21 04:38:46 +0100

    the playerGameObject moves following the pattern completed by the choise selected and hides the choises

--- levels/levelPattern/LevelPattern.cs
+++ levels/levelPattern/LevelPattern.cs
@@ -3,18 +3,11 @@ public class LevelPattern : Level
     // Start is called before the first frame update
     int index = 0;
     public UnityEngine.Sprite cover;
-    public enum directions {left, top, right, down, topLeft, topRight, downLeft, downRight};
-    public UnityEngine.Events.UnityAction[] actions = {Left, Top, Right, Down, TopLeft, TopRight, DownLeft, DownRight};
-    [System.Serializable]
-    public struct SpriteDirection
-    {
-        public UnityEngine.Sprite sprite;
-        public LevelPattern.directions direction;
-    }
-    public SpriteDirection[] spriteDirections;
-    public System.Collections.Generic.Dictionary<UnityEngine.Sprite, LevelPattern.directions> dictionary = new System.Collections.Generic.Dictionary<UnityEngine.Sprite, LevelPattern.directions>();
 
-    [UnityEngine.SerializeField]
+    public enum directions {left, top, right, down, topLeft, topRight, downLeft, downRight};
+    public SpriteVector2[] spriteVector2s;
+    public System.Collections.Generic.Dictionary<UnityEngine.Sprite, UnityEngine.Vector2> dictionary = new System.Collections.Generic.Dictionary<UnityEngine.Sprite, UnityEngine.Vector2>();
+
     public UnityEngine.GameObject[] patternsGameObject;
     public UnityEngine.GameObject[] choisesGameObject;
     public LevelPattern.Pattern[] patterns;
@@ -25,6 +18,13 @@ public class LevelPattern : Level
     #region
     // Update is called once per frame
     [System.Serializable]
+    public struct SpriteVector2
+    {
+        public UnityEngine.Sprite sprite;
+        public UnityEngine.Vector2 vector2;
+    }
+
+    [System.Serializable]
     public class Pattern
     {
         public UnityEngine.Sprite[] units;
@@ -34,7 +34,7 @@ public class LevelPattern : Level
 
     void Start()
     {
-        foreach (SpriteDirection spriteDirection in this.spriteDirections) this.dictionary.Add(spriteDirection.sprite, spriteDirection.direction);
+        foreach (SpriteVector2 spriteDirection in this.spriteVector2s) this.dictionary.Add(spriteDirection.sprite, spriteDirection.vector2);
         this.ChangeSprites();
     }
 
@@ -43,71 +43,59 @@ public class LevelPattern : Level
         for (System.Int16 index = 0; index < this.patternsGameObject.Length; index++) {
             this.patternsGameObject[index].GetComponent<UnityEngine.UI.Image>().sprite = this.patterns[this.index].units[index];
         }
-        int num = UnityEngine.Random.Range(0, this.patternsGameObject.Length);
-        this.patternsGameObject[num].GetComponent<UnityEngine.UI.Image>().sprite = this.cover;
+        System.Int16 answerIndex = (System.Int16) UnityEngine.Random.Range(0, this.patternsGameObject.Length);
+        int num = 0;
+        this.patternsGameObject[answerIndex].GetComponent<UnityEngine.UI.Image>().sprite = this.cover;
         System.Collections.Generic.List<UnityEngine.Sprite> units = new System.Collections.Generic.List<UnityEngine.Sprite>();
         units.AddRange(this.choises[this.index].units);
-        UnityEngine.Sprite unitAnswer = this.patterns[this.index].units[num];
-        foreach (UnityEngine.Sprite unit in units) {
-            if (unit == this.patterns[this.index].units[num]) {
-                unitAnswer = unit;
-            }
-        }
-        units.Remove(unitAnswer);
-        num = 0;
+        UnityEngine.Sprite answer = this.patterns[this.index].units[answerIndex];
+        this.patterns[this.index].units[answerIndex] = this.cover;
+        units.Remove(answer);
         while (this.choisesGameObject.Length > num) {
             int index = UnityEngine.Random.Range(0,(4-num));
+            this.choisesGameObject[num].SetActive(true);
             this.choisesGameObject[num].GetComponent<UnityEngine.UI.Image>().sprite = units[index];
             this.choisesGameObject[num].GetComponent<UnityEngine.UI.Button>().onClick.RemoveAllListeners();
-            this.choisesGameObject[num].GetComponent<UnityEngine.UI.Button>().onClick.AddListener(this.Wrap(units[index]));
+            this.choisesGameObject[num].GetComponent<UnityEngine.UI.Button>().onClick.AddListener(this.Wrap(answer, units[index], answerIndex));
             units.RemoveAt(index);
             num++;
         }
         num = UnityEngine.Random.Range(0,4);
-        this.choisesGameObject[num].GetComponent<UnityEngine.UI.Image>().sprite = unitAnswer;
+        this.choisesGameObject[num].GetComponent<UnityEngine.UI.Image>().sprite = answer;
         this.choisesGameObject[num].GetComponent<UnityEngine.UI.Button>().onClick.RemoveAllListeners();
-        this.choisesGameObject[num].GetComponent<UnityEngine.UI.Button>().onClick.AddListener(this.Wrap(unitAnswer));
+        this.choisesGameObject[num].GetComponent<UnityEngine.UI.Button>().onClick.AddListener(this.Wrap(answer, answer, answerIndex));
     }
-    public UnityEngine.Events.UnityAction Wrap(UnityEngine.Sprite pSprite)
+    public UnityEngine.Events.UnityAction Wrap(UnityEngine.Sprite pAnswer, UnityEngine.Sprite pSprite, System.Int16 pIndex)
     {
         return () => {
-                this.actions[(int)this.dictionary[pSprite]]();
-                this.index++;
-                this.ChangeSprites();
+                if (pAnswer == pSprite) UnityEngine.Debug.Log("win");
+                this.patterns[this.index].units[pIndex] = pSprite;
+                this.patternsGameObject[pIndex].GetComponent<UnityEngine.UI.Image>().sprite = pSprite;
+                System.Collections.Generic.List<UnityEngine.Vector2> vectors2 = new System.Collections.Generic.List<UnityEngine.Vector2>();
+                foreach (UnityEngine.Sprite sprite in this.patterns[this.index].units) {
+                    vectors2.Add(this.dictionary[sprite]);
+                }
+                foreach (UnityEngine.GameObject choiseGameObject in this.choisesGameObject) choiseGameObject.SetActive(false);
+                this.StartCoroutine(LevelPattern.MovePlayer(vectors2, this));
             };
     }
-
-
-    public static void Left()
+    public static System.Collections.IEnumerator MovePlayer(System.Collections.Generic.List<UnityEngine.Vector2> pVector2, LevelPattern pLevelPattern)
     {
-        UnityEngine.Debug.Log("Left");
-    }
-    public static void Top()
-    {
-        UnityEngine.Debug.Log("Top");
-    }
-    public static void Right()
-    {
-        UnityEngine.Debug.Log("Right");
-    }
-    public static void Down()
-    {
-        UnityEngine.Debug.Log("Down");
-    }
-    public static void TopLeft()
-    {
-        UnityEngine.Debug.Log("TopLeft");
-    }
-    public static void DownLeft()
-    {
-        UnityEngine.Debug.Log("DownLeft");
-    }
-    public static void TopRight()
-    {
-        UnityEngine.Debug.Log("TopRight");
-    }
-    public static void DownRight()
-    {
-        UnityEngine.Debug.Log("DownRight");
+        foreach (UnityEngine.Vector2 vector2 in pVector2) {
+            System.Int16 num = 0;
+            while (num < 20) {
+                pLevelPattern.screenplay.player.transform.Translate(vector2 * 0.3f);
+                num++;
+                yield return new UnityEngine.WaitForSeconds(0.04f);
+            }
+            while (num > 0) {
+                pLevelPattern.screenplay.player.transform.Translate(-vector2 * 0.3f);
+                num--;
+                yield return new UnityEngine.WaitForSeconds(0.04f); 
+            }
+        }
+        
+        pLevelPattern.index++;
+        pLevelPattern.ChangeSprites();
     }
 }
